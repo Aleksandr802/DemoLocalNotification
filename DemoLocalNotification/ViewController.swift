9e59dece9cf12266a0e34a2edb45e220cc8d5b5c
Aleksandr802 <savs802@gmail.com>
2024-01-02 15:02:00 -0800

    completed

--- DemoLocalNotification/ViewController.swift
+++ DemoLocalNotification/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkForPermission()
     }
 
+    func checkForPermission() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized:
+                self.dispatchNotification()
+            case .notDetermined:
+                notificationCenter.requestAuthorization(options: [.alert, .sound]) {didAllow,error in
+                    if didAllow {
+                        self.dispatchNotification()
+                    }
+                }
+            case .denied:
+                return
+            default:
+                return
+            }
+        }
+    }
+    
+    func dispatchNotification() {
+        let identifier = "my-morning-notification"
+        let title = "Time to work out!"
+        let body = "Don't be a lazy little butt!"
+        let hour = 14
+        let minute = 49
+        let isDaily = true
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents(calendar: calendar, timeZone: .current)
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: isDaily)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
+        notificationCenter.add(request)
+    }
 
 }
 
