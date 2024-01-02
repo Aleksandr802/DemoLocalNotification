b40ed66e06e6cd0d5d335a7bf55586cf5c01aec4
Aleksandr802 <savs802@gmail.com>
2024-01-02 13:58:50 -0800

    Initial Commit

---
+++ DemoLocalNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoLocalNotification
+//
+//  Created by Oleksandr Seminov on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
